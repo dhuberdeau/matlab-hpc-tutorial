f47321d719ac41bb2602449c8aa05206391f7eed
David Huberdeau of group turk-browne <dmh87@milgram1.milgram.hpc.yale.internal>
2019-04-12 15:23:18 -0400

    Add parallel example

--- code/example_parallel.m
+++ code/example_parallel.m
@@ -12,52 +12,23 @@
 %%%                   Finance API is not available
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Start a stopwatch timer we'll use to see how much time this script takes
-% to run
+%% run computations on only one core:
 tic
+a = nan(1, 1000);
+for idx = 1:1000
+    a(idx) = norm(eig(rand(100)));
+end
+exec_time_serial = toc;
 
-% Set company ticker, e.g. 'AAPL' for Apple
-company_ticker = 'AAPL';
+%% start a parallel pool:
+p = parpool(4);
 
-% Use the external function to download Google finance data for the selected period
-% Change this to [...] = file_data_source('AAPL') if the google_finance_data returns an error
-[hist_date, hist_high, hist_low, hist_open, hist_close, hist_vol] = file_data_source(company_ticker);
+%% run computations in parallel:
+tic
+b = nan(1, 1000);
+parfor idx = 1:1000
+    b(idx) = norm(eig(rand(100)));
+end
+exec_time_par = toc;
 
-% Convert string dates for plotting
-dates = datenum(hist_date);
-
-% 2D Plot: dates on X axis and closing prices on Y axis
-f = figure('visible','off');
-plot(dates, hist_close);
-
-% Set format for X axis tickers and labels/titles
-dateaxis('x', 17);
-xlabel('Date');
-ylabel('Price (USD)');
-title(sprintf('Closing stock prices for %s between %s and %s ', company_ticker, hist_date(1), hist_date(end)));
-
-% Save plot to PDF
-saveas(f, 'example1-2dplot.pdf');
-
-% Also save plot to black and white EPS, with 300 DPI
-dateaxis('x', 6);
-print(f, '-deps', '-r300', 'example1-2dplot.eps');
-
-% 3D Scatter plot: dates on X axis, closing prices on Y axis, volume of shares on Z axis
-% Furthermore, print in a different color datapoints where the closing
-% share price was above 100 (hm, in 2014 I did the split at 500..)
-f = figure('visible','off');
-scatter3(dates, hist_close, hist_vol, [] , hist_close > 100);
-
-% Set format for X axis tickers and labels/titles
-dateaxis('x', 17);
-xlabel('Date');
-ylabel('Price (USD)');
-zlabel('Trading volume');
-title(sprintf('Closing stock prices and trading volumes for %s between %s and %s ', company_ticker, hist_date(1), hist_date(end)));
-
-% Save plot to color EPS, with 300 DPI
-print(f, '-depsc', '-r300', 'example1-scatter.eps');
-
-% Show how much time everything took
-toc
+p.delete
